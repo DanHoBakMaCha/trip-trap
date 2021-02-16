b99aed8ca59c1338d390f48b436d354e5fc226e9
DanHoBakMaCha <56007324+DanHoBakMaCha@users.noreply.github.com>
2021-02-16 19:18:57 +0900

    #1 프로젝트 생성
    예약 사이트 프로젝트 생성

---
+++ src/main/java/com/dan/triptrap/TripTrapApplication.java
@@ -0,0 +1,13 @@
+package com.dan.triptrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dan/triptrap/TripTrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.triptrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
